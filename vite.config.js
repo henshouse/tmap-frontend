3939afb449409cfb324f3a732244b31acc51147b
filip2cz <filda.komarek@seznam.cz>
2023-07-21 08:07:40 +0200

    this should repair something

--- vite.config.js
+++ vite.config.js
@@ -5,3 +5,18 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
 })
+module.exports = {
+  chainWebpack: config => {
+    config.module
+      .rule('js')
+      .test(/\.js$/)
+      .use('file-loader')
+      .loader('file-loader')
+      .options({
+        name: '[name].[ext]',
+        outputPath: 'js',
+        esModule: false,
+        mimetype: 'application/javascript' // Nastavení MIME typu na 'application/javascript'
+      });
+  }
+};
